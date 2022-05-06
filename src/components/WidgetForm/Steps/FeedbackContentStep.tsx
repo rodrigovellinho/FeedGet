43eef3f5f8777efc14fd98ea167ac51e6d0dd9da
unknown <rodrigo.kloeckner@gmail.com>
2022-05-06 11:17:00 -0300

    project - ok

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,8 +1,10 @@
-import { ArrowLeft } from "phosphor-react";
-import { FormEvent, useState } from "react";
-import { FeedbackType, feedbackTypes } from "..";
-import { CloseButton } from "../../CloseButton";
-import ScreenshotButton from "../ScreenshotButton";
+import { ArrowLeft } from 'phosphor-react';
+import { FormEvent, useState } from 'react';
+import { FeedbackType, feedbackTypes } from '..';
+import { api } from '../../../lib/api';
+import { CloseButton } from '../../CloseButton';
+import { Loading } from '../Loading';
+import ScreenshotButton from '../ScreenshotButton';
 
 interface FeedBackContentProps {
   feedbackType: FeedbackType;
@@ -17,14 +19,23 @@ export function FeedbackContentStep({
 }: FeedBackContentProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const feedbackTypeInfo = feedbackTypes[feedbackType];
-  const [comment, setComment] = useState("");
+  const [comment, setComment] = useState('');
+  const [isSeendingFeedback, setIsSeendingFeedback] = useState(false);
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
-    console.log({
+    setIsSeendingFeedback(true);
+    /*  console.log({
       screenshot,
       comment,
+    }); */
+
+    await api.post('/feedbacks', {
+      type: feedbackType,
+      comment,
+      screenshot,
     });
+    setIsSeendingFeedback(false);
     onFeedbackSent();
   }
 
@@ -52,7 +63,7 @@ export function FeedbackContentStep({
 
       <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
         <textarea
-          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-600 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none  scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-200 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none  scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
           onChange={(event) => setComment(event.target.value)}
         />
@@ -64,9 +75,9 @@ export function FeedbackContentStep({
           <button
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSeendingFeedback}
           >
-            Enviar feedback
+            {isSeendingFeedback ? <Loading /> : 'Enviar feedback'}
           </button>
         </footer>
       </form>
